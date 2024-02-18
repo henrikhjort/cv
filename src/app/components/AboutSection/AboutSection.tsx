6fadba7e2b991a247a0ec5e3542e666fe3da685a
Henrik <henrik@etufillari.com>
2024-02-18 22:59:25 +0200

    asdf

--- src/app/components/AboutSection/AboutSection.tsx
+++ src/app/components/AboutSection/AboutSection.tsx
@@ -9,9 +9,9 @@ const AboutSection = () => {
         <h2>about</h2>
         <div className="about-text-wrapper">
           <p className="body-text about-text">I like to build web things.</p>
-          <p className="body-text about-text">{"I'm a generalist software developer with over 5 years of experience in consulting."}</p>
-          <p className="body-text about-text">{"My comfort zone includes working with TypeScript, React, React Native, and Node.js."}</p>
-          <p className="body-text about-text">{"I can get things done for the browser, mobile, and server. Keep scrolling to see projects that I have worked on."}</p>
+          <p className="body-text about-text">{"I'm a software developer with 5+ years of experience in consulting."}</p>
+          <p className="body-text about-text">{"Currently I enjoy working with React, React Native, and Node.js."}</p>
+          <p className="body-text about-text">{"Scroll down to see projects that I have worked on."}</p>
         </div>
       </div>
       <div className="right">
